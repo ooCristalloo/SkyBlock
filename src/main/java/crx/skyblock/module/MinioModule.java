29df2894b2fce0f33ae173c0cd0ef3182af85d67
reider745 <fhgfdf574@gmail.com>
2025-10-12 18:01:31 +0300

    fix: minio build

--- src/main/java/crx/skyblock/module/MinioModule.java
+++ src/main/java/crx/skyblock/module/MinioModule.java
@@ -1,19 +1,22 @@
 package crx.skyblock.module;
 
+import io.minio.MinioClient;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class MinioModule {
 
     public void initialize() {
         try {
-            /*MinioClient minioClient = MinioClient.builder()
+            MinioClient minioClient = MinioClient.builder()
                     .endpoint("http://172.17.0.2:9000")
                     .credentials("minio_root", "minio_password_R6gzFszY$u9@>@:8")
                     .build();
 
             minioClient.listBuckets();
-            System.out.println("Connection MinIO successfully");*/
-
+            log.info("Connection MinIO successfully");
         } catch (Exception e) {
-            System.out.println("Failed connection MinIO: " + e.getMessage());
+            log.error("Failed to connect to Minio", e);
         }
     }
 }
